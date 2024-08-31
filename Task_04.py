f32cc87339621ce2318339d67fb8e62eaa2937a0
rathnayr <rathnayr@metropolia.fi>
2024-08-31 20:50:33 +0530

    question 4

--- Task_04.py
+++ Task_04.py
@@ -1,3 +1,7 @@
-Year=input("Enter an year: ")
-if Year%4:
+Year=int(input("Enter an year: "))
+if (Year%4==0 and Year%100!=0) or (Year%400==0):
+    print(f"{Year} is a Leap year")
+else:
+    print(f"{Year} is not a Leap year")
+
 
